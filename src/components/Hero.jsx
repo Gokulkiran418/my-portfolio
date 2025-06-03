aa79cf357661d897367099db64ca0a2b0988400f
Gokulkiran418 <umgokulkiran@gmail.com>
2025-06-03 13:41:44 +0530

    Add typewriter effect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
 
 export function Hero() {
   return (
@@ -9,7 +10,18 @@ export function Hero() {
         </div>
         <h1 className="text-4xl font-bold text-gray-900 dark:text-gray-100">Gokul Kiran</h1>
         <p className="text-xl mt-2 font-medium text-gray-700 dark:text-gray-300">Full-Stack Developer</p>
-        <p className="mt-4 max-w-md mx-auto text-gray-600 dark:text-gray-400">Passionate about Building Innovative Web and AI Solutions</p>
+        <p className="mt-4 max-w-md mx-auto text-black dark:text-white text-lg">
+        <TypeAnimation
+          sequence={[
+            'I innovate, build, and scale fullstack, cloud, and AI solutions. Your vision, my code. I transform your ideas into reality.',
+            1000, // Pause for 1 second after typing
+          ]}
+          wrapper="span"
+          speed={50} // Typing speed (lower is faster)
+          style={{ display: 'inline-block' }}
+          repeat={Infinity} // Loop the animation
+        />
+      </p>
         <div className="mt-6 space-x-4">
           <a href="#contact" className="inline-block bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 transition-colors duration-200">Contact Me</a>
           <a href="/images/GokulResume.pdf" target="_blank" className="inline-block bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 transition-colors duration-200">Resume</a>
